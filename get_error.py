08e1450ab04b3354b82193f2a738d96bca8698e0
day0n <niuzj0@gmail.com>
2022-12-08 21:48:28 +0800

    updata matching logic

--- get_error.py
+++ get_error.py
@@ -48,25 +48,41 @@ for i,line in enumerate(lines):
         # print("move success3")
         continue
     else: 
+        # 定义要判断的行数
+        ROW_NUM = 4
+        
         for a,line in enumerate(lines):
+
             line = str(line)
             #print(line)
-            pattern = re.compile(r'(.*)/*?error:.*')
-            matches = pattern.match(line)
-            if NAME in line:
+            #匹配逻辑
+            #pattern = re.compile(r'(.*)/*?error:.*')
+            #pattern = re.compile(r'^.*\|.*$')
+            pattern = re.compile(r'^.*~~~.*$')
+            matches = pattern.match(line)   
+            #if NAME in line:
+      
+            if matches:
+                # if 'dpkg-buildpackage' in str(line):
+                #     break
 
-                if matches:
-                    if 'dpkg-buildpackage' in str(line):
-                        break
-
+                # 计算要判断的行的范围,防止出现数组越界的bug
+                start_row = max(0, a - ROW_NUM)
+                end_row = min(len(lines), a + ROW_NUM)
+                # 获取要判断的行
+                rows = str(lines[start_row:end_row])
+                # 判断是否出现了"error:"字符串   
+                if "error:" in rows:
                     print(NAME)
                     print('\n'.join(lines[a-12:a+4]))
                     print('\n')
-                    
-                        #判断faild数目是否正确
-                    faild_num = faild_num+1
-                    break
                 
+                    #判断faild数目是否正确
+                faild_num = faild_num+1
+                break
+        #test 单独测试某一个包需要在此加break，不然无限循环
+        #break;       
 print('\n')
 print(faild_num)
 print('\n')
+    
